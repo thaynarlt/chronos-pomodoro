77476f1f012e7a25ec7e19d5eca45972bc524c7c
thaynarlt <tatarlt7@gmail.com>
2025-08-06 15:24:36 -0300

    components

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+//PascalCase
 
-function App() {
-  const [count, setCount] = useState(0)
+import { Heading } from './components/Heading';
 
+import './styles/global.css';
+import './styles/theme.css';
+
+
+export function App() {
+  console.log('Oi');
   return (
+    // <> React Fragment
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+      <Heading />
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Optio nemo
+        iusto voluptates maxime doloribus? Iste aperiam neque explicabo commodi
+        mollitia at magni culpa perferendis! Repellendus repellat itaque sequi
+        odit est.
       </p>
     </>
-  )
+  );
 }
-
-export default App
---
+++ src/components/Heading.tsx
@@ -0,0 +1,9 @@
+import styles from './Heading.module.css';
+
+export function Heading() {
+  return (
+    <h1 className={styles.heading}>
+      Olá mundo!
+    </h1>
+  );
+}
